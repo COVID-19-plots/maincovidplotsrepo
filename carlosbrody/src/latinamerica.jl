0c7491f36861d321ab9eab5b0cb350f3bf7245ab
Carlos Brody <carlosbrody@gmail.com>
2020-05-24 00:21:52 -0400

    tweak

--- carlosbrody/src/latinamerica.jl
+++ carlosbrody/src/latinamerica.jl
@@ -17,8 +17,8 @@ plotGrowth(vcat(la, ["Italy", "World other than China"]), days_previous=15,
    yticks=0:10:40, ylim2=45, fname="laGrowthRate", fignum=15)
 plotAligned(vcat(la, "Italy"), fname="laAligned",
    mintic=100, maxtic=100000, fignum=16, minval=10)
-plotNew(vcat(la, allLA), fignum=17, fname="laNew", maxtic=1000, minval=1, mintic=1)
-plotNew(la, db=D, minval=1, mintic=1, maxtic = 100,
+plotNew(vcat(la, allLA), fignum=17, fname="laNew", maxtic=40000, minval=1, mintic=1)
+plotNew(la, db=D, minval=1, mintic=1, maxtic = 1000,
    counttype="deaths", fname="laNewDeaths")
 
 ##
