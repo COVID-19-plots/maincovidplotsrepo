5445270093d76919ebcdb8d6f12cbb984a3e92ce
Carlos Brody <carlosbrody@gmail.com>
2020-05-25 00:09:11 -0400

    daily update

--- carlosbrody/src/latinamerica.jl
+++ carlosbrody/src/latinamerica.jl
@@ -16,7 +16,7 @@ plotCumulative(la, fname="laCumulative", fignum=8, minval=100,
 plotGrowth(vcat(la, ["Italy", "World other than China"]), days_previous=15,
    yticks=0:10:40, ylim2=45, fname="laGrowthRate", fignum=15)
 plotAligned(vcat(la, "Italy"), fname="laAligned",
-   mintic=100, maxtic=100000, fignum=16, minval=10)
+   mintic=100, maxtic=400000, fignum=16, minval=10)
 plotNew(vcat(la, allLA), fignum=17, fname="laNew", maxtic=40000, minval=1, mintic=1)
 plotNew(la, db=D, minval=1, mintic=1, maxtic = 1000,
    counttype="deaths", fname="laNewDeaths")
