21a791a2eba0076f27a2ca8e655ec73bf70a6ebc
Carlos Brody <carlosbrody@gmail.com>
2020-05-26 23:58:38 -0400

    daily update

--- carlosbrody/src/fndefs.jl
+++ carlosbrody/src/fndefs.jl
@@ -133,6 +133,10 @@ A = setValue(A, ("Hubei", "China"), "5/23/20", 67803)
 D = setValue(D, ("Hubei", "China"), "5/23/20", 3222)
 A = setValue(A, ("Hubei", "China"), "5/24/20", 67803)
 D = setValue(D, ("Hubei", "China"), "5/24/20", 3222)
+A = setValue(A, ("Hubei", "China"), "5/25/20", 67803)
+D = setValue(D, ("Hubei", "China"), "5/25/20", 3222)
+A = setValue(A, ("Hubei", "China"), "5/26/20", 67803)
+D = setValue(D, ("Hubei", "China"), "5/26/20", 3222)
 
 
 # Write out the database with the states consolidated
