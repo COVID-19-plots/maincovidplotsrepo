37c189620ae4411e98803e0a2c2fdfb51a39182b
Carlos Brody <carlosbrody@gmail.com>
2020-07-22 00:08:53 -0400

    regular update

--- carlosbrody/src/fndefs.jl
+++ carlosbrody/src/fndefs.jl
@@ -243,6 +243,10 @@ A = setValue(A, ("Hubei", "China"), "7/17/20", 67803)
 D = setValue(D, ("Hubei", "China"), "7/17/20", 3222)
 A = setValue(A, ("Hubei", "China"), "7/18/20", 67803)
 D = setValue(D, ("Hubei", "China"), "7/18/20", 3222)
+A = setValue(A, ("Hubei", "China"), "7/19/20", 67803)
+D = setValue(D, ("Hubei", "China"), "7/19/20", 3222)
+A = setValue(A, ("Hubei", "China"), "7/20/20", 67803)
+D = setValue(D, ("Hubei", "China"), "7/20/20", 3222)
 
 
 
